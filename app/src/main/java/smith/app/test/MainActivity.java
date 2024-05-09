a999e09a159e352fb962c772099d9804c205ff1e
Hussein Shakir <husseinshakir81@gmail.com>
2024-05-09 11:20:57 +0300

    ✨ [2024-05-09] style: introduce example code in :app module

--- app/src/main/java/smith/app/test/MainActivity.java
+++ app/src/main/java/smith/app/test/MainActivity.java
@@ -1,14 +1,25 @@
 package smith.app.test;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import smith.app.test.databinding.ActivityMainBinding;
+import smith.lib.net.SImageLoader;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SImageLoader.with(this)
+                .load("https://avatars.githubusercontent.com/u/70884742?v=4")
+                .placeholder(smith.lib.net.R.drawable.downloading)
+                .onErrorLoadingImage((error) -> Log.e("Error", error.getMessage()))
+                .into(binding.imageView);
     }
 }

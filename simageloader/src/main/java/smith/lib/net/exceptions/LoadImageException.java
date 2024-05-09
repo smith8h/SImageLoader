9543bcd7f8c53eb51f340526298823d10f3b5d93
Hussein Shakir <husseinshakir81@gmail.com>
2024-05-09 12:13:57 +0300

    🧵 [2024-05-09] core: setup github actions

--- simageloader/src/main/java/smith/lib/net/exceptions/LoadImageException.java
+++ simageloader/src/main/java/smith/lib/net/exceptions/LoadImageException.java
@@ -1,6 +1,17 @@
 package smith.lib.net.exceptions;
 
+/**
+ * The exception class thrown when an error occurred while loading the image.
+ * @author <a href="https://t.me/smithdev">Hussein Shakir</a>
+ * @since 1.0.0
+ */
 public class LoadImageException extends Exception {
+
+    /**
+     * The exception class thrown when an error occurred while loading the image.
+     * @author <a href="https://t.me/smithdev">Hussein Shakir</a>
+     * @since 1.0.0
+     */
     public LoadImageException(String message) {
         super(message);
     }

1f2890547344334a080c2f3afface1f7271a6ab4
Hussein Shakir <husseinshakir81@gmail.com>
2024-05-09 11:08:59 +0300

    ✨ [2024-05-09] feat: introduce loader class

---
+++ simageloader/src/main/java/smith/lib/net/SImageLoader.java
@@ -0,0 +1,72 @@
+package smith.lib.net;
+
+import android.app.Activity;
+import android.graphics.*;
+import android.os.*;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import java.net.*;
+import java.util.concurrent.Executors;
+
+public class SImageLoader {
+
+    private Activity activity;
+    private String url;
+    private Bitmap bitmap = null;
+    private OnErrorLoadListener listener;
+    private int placeholder = R.drawable.downloading;
+
+    @NonNull
+    public static SImageLoader with(Activity activity) {
+        SImageLoader loader = new SImageLoader();
+        loader.activity = activity;
+        return loader;
+    }
+
+    public SImageLoader load(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public SImageLoader placeholder(int placeholder) {
+        this.placeholder = placeholder;
+        return this;
+    }
+
+    public SImageLoader onErrorLoadingImage(OnErrorLoadListener listener) {
+        this.listener = listener;
+        return this;
+    }
+
+    public void into(ImageView imageView) {
+        loadImage(imageView);
+    }
+
+    private void loadImage(ImageView imageView) {
+        Executors.newSingleThreadExecutor().execute(() -> {
+            try {
+                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                bitmap = BitmapFactory.decodeStream(connection.getInputStream());
+            } catch (Exception e) {
+                if (listener != null) listener.onError(new LoadImageException(e.getMessage()));
+            }
+            new Handler(Looper.getMainLooper()).post(() -> {
+                if (bitmap != null) activity.runOnUiThread(() -> imageView.setImageBitmap(bitmap));
+                else imageView.setImageResource(placeholder);
+                imageView.setClipToOutline(true);
+            });
+        });
+    }
+
+    public interface OnErrorLoadListener {
+        void onError(LoadImageException error);
+    }
+
+    public static class LoadImageException extends Exception {
+        public LoadImageException(String message) {
+            super(message);
+        }
+    }
+}
